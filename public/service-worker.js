a83e569a09aeaa06cdc99c621ab61b0573a2a1fb
amata7 <adrianmata@live.com>
2021-05-10 14:56:28 -0700

    manifest working correctly now

---
+++ public/service-worker.js
@@ -0,0 +1,11 @@
+const CACHE_NAME = "static-cache-v2";
+
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/index.js",
+  "/manifest.webmanifest",
+  "/styles.css",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+];
